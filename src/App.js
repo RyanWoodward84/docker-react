4ae5d074ef1512f468f2c91de50ea347092785ab
Ryan Woodward <rwoodward84@gmail.com>
2021-01-11 14:00:11 -0800

    Testing out git repo name change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I was changed again and again without having to refresh!
+          Testing out changing git repository name
         </p>
         <a
           className="App-link"
